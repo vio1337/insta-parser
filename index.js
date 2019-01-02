543d1d8cbaf6c6deaa1c84734e64180abea68f1a
violet moon <violet.wmoon@gmail.com>
2019-01-02 01:22:29 -0600

    condensed fetch to 10 imgs

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fetch = require('node-fetch')
 require('dotenv/config')
 
-fetchProfile = async () => {
-	let r = await fetch(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${process.env.TOKEN}`)
-	let data = await r.json()
-	return data.data
-}
+DATA = []
 
+fetch(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${process.env.TOKEN}`)
+.then(r=> r.json())
+.then(r=> r.data)
+.then(r=> {
+	for (let i=0; i<=9; i++) {
+		DATA.push(r[i].images.standard_resolution.url)
+	}
+})
 

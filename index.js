29d19234adc97fa16ba486e80843c39ac092f958
violet moon <violet.wmoon@gmail.com>
2019-01-02 01:31:36 -0600

    finished parser and added html tags

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const fetch = require('node-fetch')
 require('dotenv/config')
 
-DATA = []
-
 fetch(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${process.env.TOKEN}`)
 .then(r=> r.json())
 .then(r=> r.data)
 .then(r=> {
+	const arr = []
 	for (let i=0; i<=9; i++) {
-		DATA.push(r[i].images.standard_resolution.url)
+		arr.push(r[i].images.standard_resolution.url)
 	}
+	return arr
 })
-
+.then(r=> {
+	let url = r.map(url=> {
+		return `<img class="insta-pic" src="${url}"/>`
+	})
+	console.log(url)
+})

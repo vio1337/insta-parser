38cc83465b0fccc9b93058a4d27be7bd24d3099b
violet moon <violet.wmoon@gmail.com>
2019-01-02 21:15:51 -0600

    added html template and fixed parser

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ fetch(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${proc
 .then(r=> r.data)
 .then(r=> {
 	const arr = []
-	for (let i=0; i<=9; i++) {
-		arr.push(r[i].images.standard_resolution.url)
+	let obj = {}
+	for (let i=0; i<=8; i++) {
+		let photo = r[i].images.standard_resolution.url
+		let link = r[i].link
+		obj[photo] = link
+		arr.push(obj)
 	}
 	return arr
 })
 .then(r=> {
-	let url = r.map(url=> {
+	let url = Object.keys(r).map((photo,i)=> {
 		return `<img class="insta-pic" src="${url}"/>`
 	})
-	console.log(url)
+	console.log(r)
 })
